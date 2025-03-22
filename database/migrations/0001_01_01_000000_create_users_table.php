d97cd2cb40c037eb7c556e7621eae756533ccb23
AbdurOsit <abdurrosyidmojokertopuri@gmail.com>
2025-03-22 19:21:43 +0700

    menambahkan column id di migrasi table users

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
+            $table->id();
             $table->string('uid')->unique();
             $table->string('username');
             $table->string('jurusan');
