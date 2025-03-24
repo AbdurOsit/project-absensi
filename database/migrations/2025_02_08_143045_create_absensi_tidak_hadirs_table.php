146fcf3cc2bc664c4e4d78a02f0a3892d91c3975
AbdurOsit <abdurrosyidmojokertopuri@gmail.com>
2025-03-24 20:24:58 +0700

    menghapus column uid pada table_absesi_tidak_hadir

--- database/migrations/2025_02_08_143045_create_absensi_tidak_hadirs_table.php
+++ database/migrations/2025_02_08_143045_create_absensi_tidak_hadirs_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('absensi_tidak_hadirs', function (Blueprint $table) {
             $table->id();
-            $table->string('uid');
             $table->string('username');
             $table->foreignId('role_id');
             $table->string('kelas');
