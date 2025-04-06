a450686a71f86624a3ab62ca829dbe60ff7fd9dd
jiichiro <jiichiro@proton.me>
2025-04-06 19:30:05 +0700

    idk what i'm doing

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,26 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: 'class',
     content: [
-        
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
     ],
     theme: {
         extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                oswald: ['oswald']
+            },
+            screens: {
+                'sm': '640px',
+                'md': '768px',
+                'lg': '1024px',
+                'xl': '1280px',
+            },
         },
     },
     plugins: [],
