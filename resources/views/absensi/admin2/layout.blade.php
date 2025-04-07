1ae2316ba5620ca8600f25e9dcd42ceb0f03b793
Jiichiro <jiichiro@proton.me>
2025-04-07 19:19:25 +0700

    idk

--- resources/views/absensi/admin2/layout.blade.php
+++ resources/views/absensi/admin2/layout.blade.php
@@ -3,6 +3,7 @@
 
 <head>
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Web Absensi</title>
     @vite('resources/css/app.css')
     <style>
