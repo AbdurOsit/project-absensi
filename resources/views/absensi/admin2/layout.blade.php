77833a1acf81ff18c017669e20b524f659f818d7
AbdurOsit <abdurrosyidmojokertopuri@gmail.com>
2025-04-10 10:34:52 +0700

    input icon admin

--- resources/views/absensi/admin2/layout.blade.php
+++ resources/views/absensi/admin2/layout.blade.php
@@ -76,7 +76,7 @@
                         {{-- Input Navigation --}}
                         <a href="{{ route('admin.input') }}">
                             <div class="flex items-center gap-3 text-white cursor-pointer text-lg mt-5">
-                                {{-- <svg fill="#ffffff" class="w-6 h-6" viewBox="0 0 24 24"
+                                <svg fill="#ffffff" class="w-6 h-6" viewBox="0 0 24 24"
                                     xmlns="http://www.w3.org/2000/svg">
                                     <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
                                     <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
@@ -85,10 +85,10 @@
                                             d="M20.7,5.537a1.024,1.024,0,0,1,0,1.448L8.527,19.158,3,21l1.842-5.527L17.015,3.3a1.024,1.024,0,0,1,1.448,0Z">
                                         </path>
                                     </g>
-                                </svg> --}}
-                                <svg class="w-6 h-6 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                </svg>
+                                {{-- <svg class="w-6 h-6 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 5V4a1 1 0 0 0-1-1H8.914a1 1 0 0 0-.707.293L4.293 7.207A1 1 0 0 0 4 7.914V20a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-5M9 3v4a1 1 0 0 1-1 1H4m11.383.772 2.745 2.746m1.215-3.906a2.089 2.089 0 0 1 0 2.953l-6.65 6.646L9 17.95l.739-3.692 6.646-6.646a2.087 2.087 0 0 1 2.958 0Z"/>
-                                  </svg>
+                                  </svg> --}}
                                 <span class="titles" >Input Data</span>
                             </div>
                         </a>
@@ -237,9 +237,19 @@
                         {{-- Input Navigation --}}
                         <a href="{{ route('admin.input') }}">
                             <div class="flex items-center gap-3 text-white cursor-pointer text-lg mt-5">
-                                <svg class="w-6 h-6 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                <svg fill="#ffffff" class="w-6 h-6" viewBox="0 0 24 24"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                                    <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                                    <g id="SVGRepo_iconCarrier">
+                                        <path
+                                            d="M20.7,5.537a1.024,1.024,0,0,1,0,1.448L8.527,19.158,3,21l1.842-5.527L17.015,3.3a1.024,1.024,0,0,1,1.448,0Z">
+                                        </path>
+                                    </g>
+                                </svg>
+                                {{-- <svg class="w-6 h-6 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 5V4a1 1 0 0 0-1-1H8.914a1 1 0 0 0-.707.293L4.293 7.207A1 1 0 0 0 4 7.914V20a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-5M9 3v4a1 1 0 0 1-1 1H4m11.383.772 2.745 2.746m1.215-3.906a2.089 2.089 0 0 1 0 2.953l-6.65 6.646L9 17.95l.739-3.692 6.646-6.646a2.087 2.087 0 0 1 2.958 0Z"/>
-                                  </svg>
+                                  </svg> --}}
                                 <span>Input Data</span>
                             </div>
                         </a>
