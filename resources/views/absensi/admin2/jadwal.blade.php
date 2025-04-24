0f2fcd9e957181fa350f3b74392ba8b94e521090
AbdurOsit <abdurrosyidmojokertopuri@gmail.com>
2025-04-24 07:51:45 +0700

    remove Tugas

--- resources/views/absensi/admin2/jadwal.blade.php
+++ resources/views/absensi/admin2/jadwal.blade.php
@@ -7,9 +7,9 @@
     @endif
     
     <!-- Main Title -->
-    <h1 class="text-2xl font-semibold mb-8 dark:text-white">Input Tugas, Praktek, dan Kegiatan</h1>
+    <h1 class="text-2xl font-semibold mb-8 dark:text-white">Input Praktek dan Kegiatan</h1>
     <!-- Data Tugas -->
-    <div class="w-full mt-6 tugas">
+    {{-- <div class="w-full mt-6 tugas">
         <div class="mb-3 flex flex-col md:flex-row md:items-center md:justify-start gap-3">
             <h2 class="dark:text-white text-xl mb-3 font-semibold">Table Tugas</h2>
             <a href="/tugas/create" class="bg-purple-600 px-5 py-2 ml-2 rounded-xl text-white font-bold button text-center "><button>Create</button ></a>
@@ -27,59 +27,59 @@
                     <th class="border border-gray-700 px-4 py-2">Aksi</th>
                 </tr>
             </thead>
-            <tbody>
-                @php
+            <tbody> --}}
+                {{-- @php
                     $no = 1;
                 @endphp
-                @foreach ($tugas as $item)
-                    <tr>
-                        <td class="border border-gray-600 px-2 py-1 md:px-4 md:py-2 text-sm md:text-base">{{ $no }}</td>
-                        <td class="border border-gray-600 px-2 py-1 md:px-4 md:py-2 text-sm md:text-base">{{ $item->hari }}</td>
-                        <td class="border border-gray-600 px-2 py-1 md:px-4 md:py-2 text-sm md:text-base">{{ \Carbon\Carbon::parse($item->tanggal)->format('d M Y') }}</td>
-                        <td class="border border-gray-600 px-2 py-1 md:px-4 md:py-2 text-sm md:text-base">{{ $item->tugas }}</td>
-                        <td class="border border-gray-600 px-2 py-1 md:px-4 md:py-2 text-sm md:text-base">{{ $item->deadline_hari }}</td>
-                        <td class="border border-gray-600 px-2 py-1 md:px-4 md:py-2 text-sm md:text-base">{{ \Carbon\Carbon::parse($item->deadline_tanggal)->format('d M Y') }}</td>
-                        <td class="border border-gray-600 px-2 py-1 md:px-4 md:py-2 text-sm md:text-base text-center flex justify-center">
+                @foreach ($tugas as $item) --}}
+                    {{-- <tr> --}}
+                        {{-- <td class="border border-gray-600 px-2 py-1 md:px-4 md:py-2 text-sm md:text-base">{{ $no }}</td> --}}
+                        {{-- <td class="border border-gray-600 px-2 py-1 md:px-4 md:py-2 text-sm md:text-base">{{ $item->hari }}</td> --}}
+                        {{-- <td class="border border-gray-600 px-2 py-1 md:px-4 md:py-2 text-sm md:text-base">{{ \Carbon\Carbon::parse($item->tanggal)->format('d M Y') }}</td> --}}
+                        {{-- <td class="border border-gray-600 px-2 py-1 md:px-4 md:py-2 text-sm md:text-base">{{ $item->tugas }}</td> --}}
+                        {{-- <td class="border border-gray-600 px-2 py-1 md:px-4 md:py-2 text-sm md:text-base">{{ $item->deadline_hari }}</td> --}}
+                        {{-- <td class="border border-gray-600 px-2 py-1 md:px-4 md:py-2 text-sm md:text-base">{{ \Carbon\Carbon::parse($item->deadline_tanggal)->format('d M Y') }}</td> --}}
+                        {{-- <td class="border border-gray-600 px-2 py-1 md:px-4 md:py-2 text-sm md:text-base text-center flex justify-center"> --}}
 
                             {{-- Update Icon --}}
-                            <a class="font-bold text-lg text-white" href="{{ route('tugas.update',  $item->id) }}">
-                                <svg class="w-6 h-6 bg-blue-700 dark:bg-blue-500" aria-hidden="true"
-                                    xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
-                                    viewBox="0 0 24 24">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                        stroke-width="2"
-                                        d="M18 14v4.833A1.166 1.166 0 0 1 16.833 20H5.167A1.167 1.167 0 0 1 4 18.833V7.167A1.166 1.166 0 0 1 5.167 6h4.618m4.447-2H20v5.768m-7.889 2.121 7.778-7.778" />
-                                </svg>
-                            </a>
+                            {{-- <a class="font-bold text-lg text-white" href="{{ route('tugas.update',  $item->id) }}"> --}}
+                                {{-- <svg class="w-6 h-6 bg-blue-700 dark:bg-blue-500" aria-hidden="true" --}}
+                                    {{-- xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" --}}
+                                    {{-- viewBox="0 0 24 24"> --}}
+                                    {{-- <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" --}}
+                                        {{-- stroke-width="2" --}}
+                                        {{-- d="M18 14v4.833A1.166 1.166 0 0 1 16.833 20H5.167A1.167 1.167 0 0 1 4 18.833V7.167A1.166 1.166 0 0 1 5.167 6h4.618m4.447-2H20v5.768m-7.889 2.121 7.778-7.778" /> --}}
+                                {{-- </svg> --}}
+                            {{-- </a> --}}
 
                             {{-- Delete Icon --}}
-                            <form action="{{ route('tugas.delete', $item->id) }}" method="POST" class="font-bold text-lg text-white">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit">
-                                    <svg class="w-6 h-6 bg-red-700 dark:bg-red-500" aria-hidden="true"
-                                        xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
-                                        viewBox="0 0 24 24">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                            stroke-width="2"
-                                            d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z" />
-                                    </svg>
-                                </button>
-                            </form>
-                        </td>
-                    </tr>
-                    @php
-                        $no++;
-                    @endphp
-                @endforeach
-            </tbody>
-        </table>
-    </div>
+                            {{-- <form action="{{ route('tugas.delete', $item->id) }}" method="POST" class="font-bold text-lg text-white"> --}}
+                                {{-- @csrf --}}
+                                {{-- @method('DELETE') --}}
+                                {{-- <button type="submit"> --}}
+                                    {{-- <svg class="w-6 h-6 bg-red-700 dark:bg-red-500" aria-hidden="true" --}}
+                                        {{-- xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" --}}
+                                        {{-- viewBox="0 0 24 24"> --}}
+                                        {{-- <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" --}}
+                                            {{-- stroke-width="2" --}}
+                                            {{-- d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z" /> --}}
+                                    {{-- </svg> --}}
+                                {{-- </button> --}}
+                            {{-- </form> --}}
+                        {{-- </td> --}}
+                    {{-- </tr> --}}
+                    {{-- @php --}}
+                        {{-- $no++; --}}
+                    {{-- @endphp --}}
+                {{-- @endforeach --}}
+            {{-- </tbody> --}}
+        {{-- </table> --}}
+    {{-- </div> --}}
         <!-- Pagination -->
-        <div class="mt-4">
+        {{-- <div class="mt-4">
             {{ $tugas->appends(['tugas_page' => request('tugas_page'),'praktek_page' => request('praktek_page'), 'kegiatan_page' => request('kegiatan_page')])->links('pagination::tailwind') }}
         </div>
-    </div>
+    </div> --}}
 
     <!-- Data Praktek -->
     <div class="w-full mt-6 praktek">
